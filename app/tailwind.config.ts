bf0fde53b9d3400921b8c9983f8a06b1c91fbe64
Saban-94 <hsaban2025@gmail.com>
2026-02-09 10:04:53 +0200

    Create tailwind.config.ts

---
+++ app/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          dark: "#0F172A",
+          primary: "#1E3A8A",
+          secondary: "#14B8A6",
+        }
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
+export default config;
