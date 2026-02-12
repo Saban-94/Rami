a4d2066f9c1f26b8ab4a5b9dc511aed75aa02cf0
Saban-94 <hsaban2025@gmail.com>
2026-02-12 10:38:53 +0200

    Update page.tsx

--- app/trial/page.tsx
+++ app/trial/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = "force-dynamic";
 import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import Navigation from "../../components/Navigation";
