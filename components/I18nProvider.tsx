1a9ced83d647cd7114b4efa7e6331feeb786ebe9
Saban-94 <hsaban2025@gmail.com>
2026-02-16 03:12:31 +0200

    Update I18nProvider.tsx

--- components/I18nProvider.tsx
+++ components/I18nProvider.tsx
@@ -17,3 +17,8 @@ export const I18nProvider = ({ children }: { children: React.ReactNode }) => {
     </I18nContext.Provider>
   );
 };
+export const useI18n = () => {
+  const context = useContext(I18nContext);
+  if (!context) throw new Error("useI18n must be used within I18nProvider");
+  return context;
+};
